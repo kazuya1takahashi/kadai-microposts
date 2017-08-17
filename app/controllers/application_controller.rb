16592cb9a4d11a2c58e1e3168a4068e05f3acddf
kazuya.takahashi <kazuya.cloud9@gmail.com>
2017-08-17 12:38:31 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   include SessionsHelper
-  
+
   private
 
   def require_user_logged_in
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
+  
 end
